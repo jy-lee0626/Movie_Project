50cf87acdc2c1e91c106e68f5675eacb5742ea19
youngchan choi <youngchan419@gmail.com>
2022-05-19 11:03:28 +0900

    create community models

--- final-pjt-back/moving/settings.py
+++ final-pjt-back/moving/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'movies',
     'community',
     'accounts',
-    
+    'django_extensions',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
