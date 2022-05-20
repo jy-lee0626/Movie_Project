ee063bc53fbfc95e8ab243e2a701629ccd2cc389
youngchan choi <youngchan419@gmail.com>
2022-05-21 01:06:46 +0900

    back 대충 완성

--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -1,32 +1,17 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer
-from .models import User
 
+from .serializers import ProfileSerializer
 
-@api_view(['POST'])
-def signup(request):
-    password = request.data.get('password')
-    passwordConfirmation = request.data.get('passwordConfirmation')
-    if password != passwordConfirmation:
-        return Response({'error': '비밀번호가 일치하지 않습니다!!!'})
-    
-    serializer = UserSerializer(data= request.data)
-
-    if serializer.is_valid(raise_exception=True) : 
-        user=serializer.save()
-        user.set_password(request.data.get('password'))
-        user.save()
-        return Response(serializer.data)
-
+User = get_user_model()
 
 @api_view(['GET'])
-@authentication_classes([JSONWebTokenAuthentication])
-@permission_classes([IsAuthenticated])
-def profile(request):
-    user = get_object_or_404(User, pk=request.user.pk)
-    serializer= UserSerializer(user)
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    serializer = ProfileSerializer(user)
     return Response(serializer.data)
+    
+
