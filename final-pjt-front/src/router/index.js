d2bc5b1b4bfc470b533b91845aded74efafd207d
jy-lee0626 <lklll369@gmail.com>
2022-05-20 16:46:16 +0900

    router

--- final-pjt-front/src/router/index.js
+++ final-pjt-front/src/router/index.js
@@ -54,7 +54,7 @@ router.beforeEach((to, from, next) => {
   const isAuthRequired = !noAuthPages.includes(to.name)
 
   if (isAuthRequired && !isLoggedIn) {
-    alert('Require Login. Redirecting..')
+    alert('로그인이 필요합니다!')
     next({ name: 'login' })
   } else {
     next()
