4525871df896835a68fa908fbc825fc31710510e
youngchan choi <youngchan419@gmail.com>
2022-05-22 16:05:36 +0900

    account 해결

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from dj_rest_auth.registration.serializers import RegisterSerializer
 from .models import User
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -8,3 +9,23 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+
+class CustomRegisterSerializer(RegisterSerializer):
+    # 기본 설정 필드: username, password, email
+    # 추가 설정 필드: profile_image
+    # profile_image = serializers.ImageField(use_url=True)
+    gender = serializers.CharField()
+
+    def get_cleaned_data(self):
+        data = super().get_cleaned_data()
+        data['gender'] = self.validated_data.get('gender')
+        # data['profile_image'] = self.validated_data.get('profile_image', '')
+
+        return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta: 
+        model = User
+        fields = '__all__'
