d0d15767b5ff660d0e77ccf3b7c6c5b68d3bda58
minji <heunhanireum@gmail.com>
2022-05-25 11:09:54 +0900

    moviecomment다하긴 했는데 오류있음

--- final-pjt-front/src/components/movies/MovieCommentForm.vue
+++ final-pjt-front/src/components/movies/MovieCommentForm.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-    <h2>CommentForm</h2>
+    <form @submit.prevent="onSubmit" class="movie-comment-form">
+      <label for="comment">comment: </label>
+      <input type="text" id="comment" v-model="content" required>
+      <button>CommentForm</button>
+    </form>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 export default {
   name: "MovieCommentForm",
+  data() {
+    return {
+      content: ''
+    }
+  }, 
+  computed: {
+    ...mapGetters(['movieDetail'])
+  },
+  methods: {
+    ...mapActions(['createMovieComment']),
+    onSubmit() {
+      this.createMovieComment({ moviePk: this.movieDetail.pk, content: this.content, })
+      this.content = ''
+    }
+  }
 }
 </script>
 
 <style>
-
+.comment-list-form {
+  border: 1px solid black;
+  margin: 1rem;
+  padding: 1rem;
+}
 </style>
